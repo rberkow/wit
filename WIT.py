e252a8d188f4e227ce1dc4849b630bd4ad125350
Ruth Berkow <ruth.berkow@mail.mcgill.ca>
2016-03-08 13:59:42 -0500

    New & improved templates

--- WIT.py
+++ WIT.py
@@ -6,5 +6,25 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('index.html')
 
+
+@app.route('/whyIT/')
+def why_IT():
+    return render_template('article.html', title="Why IT?")
+
+
+@app.route('/events/')
+def events():
+    return render_template('events.html', title="Events")
+
+@app.route('/exec/')
+def current_exec():
+    return render_template('404.html')
+
+
+@app.route('/contact/')
+def contact():
+    return render_template('404.html')
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
